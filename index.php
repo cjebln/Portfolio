729bf939c786750b013af07e38cbcb23ee9e3147
Christian Ebalin <131768163+cjebln@users.noreply.github.com>
2025-06-14 21:28:59 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,322 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Ebalin | Portfolio</title>
+	<?php include('includes/head-links.php'); ?>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Birthstone&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="/assets/css/index.css">
+</head>
+<body>
+    <?php include('includes/header.php'); ?>
+    <main>
+        <!-- HERO -->
+     <section class="hero" id="particles-js">
+        <div class="inner">
+            <div class="hero-container">
+                <div class="info">
+                    <h1>Christian Ebalin</h1>
+                    <p>I'm a <span>Frontend Developer</span></p>
+                    <div class="btn-group">
+                        <div class="btn btn-downloadcv">Download CV</div>
+                        <div class="btn btn-contact">Contact</div>
+                    </div>
+                </div>
+    
+                <img src="/assets/img/hero-img.jpg" alt="Christian Ebalin">
+            </div>
+        </div>
+     </section>
+
+    <!-- ABOUT -->
+    <section class="about">
+        <div class="inner">
+            <h2 class="section-header">About Me</h2>
+            <div class="about-container">
+                <div class="about-info">
+                    <p>Hi, I'm Christian Ebalin, a passionate Frontend Developer with a knack for creating beautiful and functional web applications. I specialize in HTML, CSS, and JavaScript, and I love turning complex problems into simple, user-friendly solutions.</p>
+                    <p>With a keen eye for design and a commitment to quality, I strive to deliver exceptional user experiences in every project I undertake.</p>
+                </div>
+                <div class="about-img">
+                    <img src="/assets/img/about-img.jpg" alt="Christian Ebalin">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- RESUME -->
+    <section class="resume">
+        <div class="inner">
+            <h2 class="section-header">Resume</h2>
+            <div class="section-container">
+                <div class="left-col">
+                    <div class="resume-item">
+                        <div class="resume-item-content">
+                            <h3>EDUCATION</h3>
+                            <p><span>Bachelor of Science in Information Technology</span></p>
+                            <h4>STI College Fairview | 2020 - 2024</h4>
+                            <ul>
+                                <li>&#8226 Best Thesis Award</li>
+                                <li>&#8226 Cum Laude</li>
+                                <li>&#8226 GWA 1.42</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="resume-item">
+                         <div class="resume-item-content">
+                            <h3>SKILLS</h3>
+                            <ul>
+                                <li>&#8226 CSS</li>
+                                <li>&#8226 JavaScript</li>
+                                <li>&#8226 JQuery</li>
+                                <li>&#8226 PHP</li>
+                                <li>&#8226 MySql</li>
+                                <li>&#8226 Github</li>
+                            </ul>
+                        </div>
+                    </div>
+                   <div class="resume-item">
+                         <div class="resume-item-content">
+                            <h3>CERTIFICATES</h3>
+                            <ul>
+                                <li>&#8226 CSS</li>
+                                <li>&#8226 JavaScript</li>
+                                <li>&#8226 JQuery</li>
+                                <li>&#8226 PHP</li>
+                                <li>&#8226 MySql</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="right-col">
+                    <div class="resume-item">
+                        <div class="resume-item-content">
+                            <h3>PROFESSIONAL SUMMARY</h3>
+                            <p class="prof-sum">Creative and detail-oriented Front-End Developer with strong proficiency in HTML, CSS,
+                            JavaScript, JQueary, PHP, and Bootstrap. Experienced in developing responsive,
+                            user-friendly web applications and interfaces with a focus on clean design and
+                            performance optimization. Adept at collaborating with cross-functional teams to
+                            deliver engaging user experiences, enhance accessibility, and maintain high coding
+                            standards. Passionate about continuous learning and applying innovative solutions to
+                            modernize applications. Seeking to contribute technical expertise and a keen eye for
+                            design to a dynamic development team.</p>
+                        </div>
+                    </div>
+                    <div class="resume-item">
+                         <div class="resume-item-content">
+                            <h3>EXPRIENCE</h3>
+                            <div class="experience-item">
+                                 <p><span>Frontend Developer | Raitech Web Dev</span></p>
+                                 <h4>May 2023 - Present</h4>
+                                <ul>
+                                    <li><p>&#8226 Developed and designed responsive web applications using HTML, CSS, Bootstrap, and JavaScript, ensuring cross-browser compatibility and optimized performance.</p></li>
+                                    <li><p>&#8226 Built engaging and interactive landing pages with HTML, CSS, and JavaScript, incorporating modern UI/UX principles for an enhanced user experience.</p></li>
+                                    <li><p>&#8226 Redesigned and upgraded an existing web application by leveraging Bootstrap for a mobile-friendly interface and PHP for backend functionality, improving efficiency and user interaction.</p></li>
+                                    <li><p>&#8226 Contributed to the development of a voting platform, focusing on front-end design, interactive elements, and seamless user navigation to enhance accessibility and usability.</p></li>
+                                    <li><p>&#8226 Designed and built a client's personal website, ensuring a visually appealing and layout tailored to the client's specific needs and branding.</p></li>
+                                    <li><p>&#8226 Collaborated with backend developers and designers to create cohesive and fully functional web applications, following best practices in software development.</p></li>
+                                </ul>
+                            </div>
+                           <div class="experience-item">
+                                <p><span class="experience-title">Graphic Designer | Cayebel Equipment Inc (Internship)</span></p>
+                                <h4>March 2024 - May 2024</h4>
+                                <ul>
+                                    <li><p>&#8226 Designed machine brochures to support marketing initiatives, ensuring visually appealing and informative content.</p></li>
+                                    <li><p>&#8226 Collaborated with the marketing team to align designs with brand guidelines and target audience preferences.</p></li>
+                                    <li><p>&#8226  Gained experience in marketing materials creation and effective communication strategies.</p></li>    
+                                </ul>
+                           </div>
+                           <div class="experience-item">
+                                <p><span class="experience-title">IT Instructor | STI College Fairview</span></p>
+                                <h4>August 2024 - Present</h4>
+                                <ul>
+                                    <li><p>&#8226 Delivered comprehensive lectures and hands-on sessions on IT major subjects, ensuring clarity and engagement for diverse student groups.</p></li>
+                                    <li><p>&#8226 Prepared course materials, assessments, and interactive activities to enhance learning outcomes.</p></li>
+                                    <li><p>&#8226 Assisted students in developing technical projects and problem-solving skills, fostering innovation and critical thinking.</p></li>
+                                    <li><p>&#8226 Provided guidance on programming languages, application development, and IT service management principles.</p></li>
+                                    <li><p>&#8226 Thesis advisor of two groups.</p></li>    
+                                </ul>
+                           </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- PORTFOLIO -->
+     <section class="portfolio">
+        <div class="inner">
+            <h2 class="section-header">Portfolio</h2>
+            <div class="social-media-post">
+                <h3>Social Media Posts</h3>
+                <div class="social-media-post-item">
+                    <img src="/assets/img/fruitshake-series.jpg" alt="">
+                    <div class="description">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore iste quaerat commodi eveniet qui eos nemo aut. </p>
+                    </div>
+                </div>
+                <div class="social-media-post-item">
+                    <img src="/assets/img/gym-ads.jpg" alt="">
+                    <div class="description">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore iste quaerat commodi eveniet qui eos nemo aut. </p>
+                    </div>
+                </div>
+                <div class="social-media-post-item">
+                    <img src="/assets/img/unlimited-rice.jpg" alt="">
+                    <div class="description">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore iste quaerat commodi eveniet qui eos nemo aut. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+     </section>
+
+     <!-- Contact -->
+      <section class="contact">
+        <div class="inner">
+            <h2 class="section-header">Contact</h2>
+            <div class="contact-container">
+                <form action="" method="post" class="contact-form">
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input type="text" id="name" name="name" placeholder="Juan Dela Cruz" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" id="email" name="email" placeholder="example@gmail.com" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="message">Message</label>
+                        <textarea id="message" name="message" rows="5" required></textarea> 
+                    </div>
+                    <div class="form-group  ">
+                        <button type="submit" class="btn btn-submit">Send Message</button>      
+                    </div>
+                </form>
+            </div>
+        </div>
+      </section>
+    </main>
+    <?php include('includes/footer.php'); ?>
+    <script src="/assets/js/jquery.js"></script>
+    <script>
+        particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#000000"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 5,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#000000",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true,
+  "config_demo": {
+    "hide_card": false,
+    "background_color": "#f4f4f4",
+    "background_image": "",
+    "background_position": "50% 50%",
+    "background_repeat": "no-repeat",
+    "background_size": "cover"
+  }
+});
+    </script>
+</body>
+</html>
